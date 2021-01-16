d42f774533a5effb8aab3f17e511a7f92374a2a6
nnkrbyok-kxxrin <ec2-user@ip-172-31-12-132.ec2.internal>
2021-01-16 06:29:55 +0000

    再提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,16 +8,12 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to "/books/#{@book.id}"
+      redirect_to book_path(@book.id)
     else
       render "index"
     end
@@ -35,7 +31,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to "/books/#{@book.id}"
+      redirect_to book_path(@book.id)
     else
       render "edit"
     end
@@ -45,7 +41,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     if book.destroy
       flash[:notice] = "Book was successfully destroyed."
-      redirect_to "/books"
+      redirect_to books_path
     end
   end
 
